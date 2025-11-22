2521112caf4572c0d3118d5e4f6e557e6fd311df
robotur <jukangu16@gmail.com>
2025-11-22 13:26:57 +0300

    Hi

--- src/main/java/net/stanley/tutorialmod/TutorialMod.java
+++ src/main/java/net/stanley/tutorialmod/TutorialMod.java
@@ -4,7 +4,7 @@ import net.fabricmc.api.ModInitializer;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+//comment test
 public class TutorialMod implements ModInitializer {
 	public static final String MOD_ID = "tutorial-mod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
