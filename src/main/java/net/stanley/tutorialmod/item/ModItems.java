bd15ad0c309959055bdbb2a1be9dbe3099190220
robotur <jukangu16@gmail.com>
2025-11-22 14:26:42 +0300

    Added Silver item. Texture not working idk why

---
+++ src/main/java/net/stanley/tutorialmod/item/ModItems.java
@@ -0,0 +1,28 @@
+package net.stanley.tutorialmod.item;
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+
+import net.minecraft.util.Identifier;
+import net.stanley.tutorialmod.TutorialMod;
+
+public class ModItems {
+
+    public static final Item SILVER_INGOT = registerItem("silver_ingot", new Item(new Item.Settings()));
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(TutorialMod.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        TutorialMod.LOGGER.info("Registering ModItems for " + TutorialMod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+            entries.add(SILVER_INGOT);
+        });
+    }
+}
